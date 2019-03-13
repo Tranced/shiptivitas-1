47779eaeb6aedbc851b02e4db1fbbbf8e989d98a
Tranced <terhyang@gmail.com>
2019-03-13 00:44:13 -0700

    Finished all tasks

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Dragula from 'dragula';
 import 'dragula/dist/dragula.css';
 import Swimlane from './Swimlane';
+import Dragula from 'react-dragula';
 import './Board.css';
 
 export default class Board extends React.Component {
@@ -21,38 +21,57 @@ export default class Board extends React.Component {
       complete: React.createRef(),
     }
   }
+  componentDidMount() {
+    this.drake = Dragula([this.swimlanes.backlog.current, this.swimlanes.inProgress.current, this.swimlanes.complete.current])
+    this.drake.on('drop', (el, source, target, sibling) => {
+      let newLane = source.previousSibling.innerText.toLowerCase();
+      el.className = 'Card';
+      if (newLane === 'backlog') {
+        el.className += ' Card-grey';
+        el.setAttribute("data-status", "Backlog")
+      } else if (newLane === 'in progress') {
+        el.className += ' Card-blue';
+        el.setAttribute("data-status", "in Progress")
+      } else if (newLane === 'complete') {
+        el.className += ' Card-green';
+        el.setAttribute("data-status", "Complete")
+      }
+    })
+  }
+
+
   getClients() {
     return [
-      ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'in-progress'],
-      ['2','Wiza LLC','Exclusive Bandwidth-Monitored Implementation', 'complete'],
-      ['3','Nolan LLC','Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
-      ['4','Thompson PLC','Streamlined Regional Knowledgeuser', 'in-progress'],
-      ['5','Walker-Williamson','Team-Oriented 6Thgeneration Matrix', 'in-progress'],
-      ['6','Boehm and Sons','Automated Systematic Paradigm', 'backlog'],
-      ['7','Runolfsson, Hegmann and Block','Integrated Transitional Strategy', 'backlog'],
-      ['8','Schumm-Labadie','Operative Heuristic Challenge', 'backlog'],
-      ['9','Kohler Group','Re-Contextualized Multi-Tasking Attitude', 'backlog'],
-      ['10','Romaguera Inc','Managed Foreground Toolset', 'backlog'],
-      ['11','Reilly-King','Future-Proofed Interactive Toolset', 'complete'],
-      ['12','Emard, Champlin and Runolfsdottir','Devolved Needs-Based Capability', 'backlog'],
-      ['13','Fritsch, Cronin and Wolff','Open-Source 3Rdgeneration Website', 'complete'],
-      ['14','Borer LLC','Profit-Focused Incremental Orchestration', 'backlog'],
-      ['15','Emmerich-Ankunding','User-Centric Stable Extranet', 'in-progress'],
-      ['16','Willms-Abbott','Progressive Bandwidth-Monitored Access', 'in-progress'],
-      ['17','Brekke PLC','Intuitive User-Facing Customerloyalty', 'complete'],
-      ['18','Bins, Toy and Klocko','Integrated Assymetric Software', 'backlog'],
-      ['19','Hodkiewicz-Hayes','Programmable Systematic Securedline', 'backlog'],
-      ['20','Murphy, Lang and Ferry','Organized Explicit Access', 'backlog'],
+      ['1', 'Stark, White and Abbott', 'Cloned Optimal Architecture', 'in-progress'],
+      ['2', 'Wiza LLC', 'Exclusive Bandwidth-Monitored Implementation', 'complete'],
+      ['3', 'Nolan LLC', 'Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
+      ['4', 'Thompson PLC', 'Streamlined Regional Knowledgeuser', 'in-progress'],
+      ['5', 'Walker-Williamson', 'Team-Oriented 6Thgeneration Matrix', 'in-progress'],
+      ['6', 'Boehm and Sons', 'Automated Systematic Paradigm', 'backlog'],
+      ['7', 'Runolfsson, Hegmann and Block', 'Integrated Transitional Strategy', 'backlog'],
+      ['8', 'Schumm-Labadie', 'Operative Heuristic Challenge', 'backlog'],
+      ['9', 'Kohler Group', 'Re-Contextualized Multi-Tasking Attitude', 'backlog'],
+      ['10', 'Romaguera Inc', 'Managed Foreground Toolset', 'backlog'],
+      ['11', 'Reilly-King', 'Future-Proofed Interactive Toolset', 'complete'],
+      ['12', 'Emard, Champlin and Runolfsdottir', 'Devolved Needs-Based Capability', 'backlog'],
+      ['13', 'Fritsch, Cronin and Wolff', 'Open-Source 3Rdgeneration Website', 'complete'],
+      ['14', 'Borer LLC', 'Profit-Focused Incremental Orchestration', 'backlog'],
+      ['15', 'Emmerich-Ankunding', 'User-Centric Stable Extranet', 'in-progress'],
+      ['16', 'Willms-Abbott', 'Progressive Bandwidth-Monitored Access', 'in-progress'],
+      ['17', 'Brekke PLC', 'Intuitive User-Facing Customerloyalty', 'complete'],
+      ['18', 'Bins, Toy and Klocko', 'Integrated Assymetric Software', 'backlog'],
+      ['19', 'Hodkiewicz-Hayes', 'Programmable Systematic Securedline', 'backlog'],
+      ['20', 'Murphy, Lang and Ferry', 'Organized Explicit Access', 'backlog'],
     ].map(companyDetails => ({
       id: companyDetails[0],
       name: companyDetails[1],
       description: companyDetails[2],
-      status: companyDetails[3],
     }));
   }
+
   renderSwimlane(name, clients, ref) {
     return (
-      <Swimlane name={name} clients={clients} dragulaRef={ref}/>
+      <Swimlane name={name} clients={clients} dragulaRef={ref} />
     );
   }
 
